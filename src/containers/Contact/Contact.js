a83254aa21cb401ec371a3aff508e7b621e510e2
Ignacio Mansilla Derqui <ignacio.mansilla@qmenta.com>
2021-08-22 08:08:05 +0200

    [DOSELVA] build stories & contact page

--- src/containers/Contact/Contact.js
+++ src/containers/Contact/Contact.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import styles from './Contact.module.scss';
-
 import Header from '../../components/Header/Header'
+import Footer from '../../components/Footer/Footer'
+import dosselvamano from '../../assets/images/doselva_mano2.png';
 
 const Contact = () => {
 
     return (
       <div className={styles.container}>
-          <Header background='green'/>
-        <p>Contact</p>        
+        <Header background='green'/>
+        <div className={styles.contactContainer}>
+          <div className={styles.text}>
+            <h1>
+              Doselva is actively seeking new, <br/>
+              long-term relationships with companies <br/>
+              interested in sustainable sourcing <br/>
+              of turmeric, ginger, cardamom, vanilla, <br/>
+              lemon grass, hibiscus, and more.
+            </h1>
+            <p className={styles.subtitle}>Please call for our technical speciﬁcations, price quote or for samples.</p>
+          </div>
+        </div>
+        <div className={styles.contactSection}>
+          <div className={styles.data}>
+            <h1 className={styles.mail}>
+              Contact <br/>
+              <a href='mailto:info@doselva.com' target="_blank" rel="noreferrer">info@doselva.com</a>
+
+            </h1>
+            <h1 className={styles.whatsapp}>
+              Whatsapp or call <br/>
+              <a href="https://api.whatsapp.com/send?phone=50586815356&text=Hello" target="_blank" rel="noreferrer">+505 8681 5356</a>
+            </h1>
+          </div>
+          <div className={styles.image}>
+            <img src={dosselvamano} alt="hand"/>
+          </div>
+        </div>
+        <Footer/>       
       </div>
     )
   }
--- src/containers/Stories/Stories.js
+++ src/containers/Stories/Stories.js
@@ -1,14 +1,106 @@
 import React from 'react';
 import styles from './Stories.module.scss';
-
 import Header from '../../components/Header/Header'
+import Footer from '../../components/Footer/Footer'
+import news from '../../assets/images/Doselva_About_Us_04.png';
+import forest from '../../assets/images/Doselva_About_Us_05.png';
+import {NavLink} from 'react-router-dom';
+
 
 const Stories = () => {
 
     return (
       <div className={styles.container}>
-          <Header background='green'/>
-        <p>Stories</p>        
+        <Header background='green'/>
+        <div className={styles.storiesContainer}>
+          <div className={styles.row}>
+            <div className={styles.column}>
+              <div className={styles.image}>
+                <img src={news} alt='news'/>
+                <div className={styles.text}>
+                  <h1>Social Impact</h1>
+                  <p>
+                    April 8, 2021 <br/>
+                    by Doselva
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className={styles.column}>
+              <div className={styles.columnRow}>
+                <div className={`${styles.columnRowOne} ${styles.red}`}>
+                  <div className={styles.text}>
+                    <h1>
+                      Jefferson’s journey and Doselva’s <br/>
+                      founding story
+                    </h1>
+                    <p>
+                      April 8, 2021 <br/>
+                      by Doselva
+                    </p>
+                  </div>
+                </div>
+                <div className={`${styles.columnRowTwo} ${styles.green}`}>
+                  <div className={styles.text}>
+                    <h1>Spices for impact</h1>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.row}>
+            <div className={styles.column}>
+              <div className={styles.columnRow}>
+                <div className={`${styles.columnRowOne} ${styles.blue}`}>
+                  <div className={styles.text}>
+                    <h1>
+                      Jefferson’s journey and Doselva’s <br/>
+                      founding story
+                    </h1>
+                    <p>
+                      April 8, 2021 <br/>
+                      by Doselva
+                    </p>
+                  </div>
+                </div>
+                <div className={`${styles.columnRowTwo} ${styles.orange}`}>
+                  <div className={styles.text}>
+                    <h1>
+                      Grown in the <br/>
+                      forests of <br/>
+                      Central America
+                    </h1>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.column}>
+              <div className={styles.image}>
+                <img src={forest} alt='forest'/>
+                <div className={styles.text}>
+                  <h1>
+                    Agroforestry: <br/>
+                    Environmental <br/>
+                    Impact
+                  </h1>
+                  <p>
+                    April 8, 2021 <br/>
+                    by Doselva
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.buttonContainer}>
+          <NavLink 
+              exact
+              to={'/about'}
+          >
+              <p>Load more stories</p>
+          </NavLink>
+        </div>
+        <Footer/>       
       </div>
     )
   }
